2176395b22953a85981b10cc1306bb3ef3519fc2
ubiquitouss <ubiquitouss@yahoo.com>
2023-11-25 04:59:16 +0000

    initial commit

---
+++ posts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    text = models.TextField()
+
+
+    def __str__(self): # new
+     return self.text[:50]
+
+
